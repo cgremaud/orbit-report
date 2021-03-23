004c4628bc8dac12620153206670e76370745934
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-23 14:11:09 -0500

    space debris shows red warning color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,18 +15,15 @@ export class AppComponent {
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
     
-    window.fetch(satellitesUrl).then(function(response) {
-      response.json().then(function(data) {
-
+    window.fetch(satellitesUrl).then(function(response: any) {
+      response.json().then(function(data: any) {
+        console.log(data)
         let fetchedSatellites = data.satellites;
-        // TODO: loop over satellites
+
         for (let i: number = 0; i < fetchedSatellites.length; i++) {
           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellite);
         }
-        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-
       }.bind(this));
     }.bind(this));
   }
